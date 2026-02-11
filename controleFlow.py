9a773e9919cbcbbae73876ed5aa8744e20c9e338
Akshat saini <akshatsaini@Akshats-MacBook-Air.local>
2026-02-11 16:40:38 +0530

    control flow statement

---
+++ controleFlow.py
@@ -0,0 +1,75 @@
+# age=28
+
+# print("age checker")
+# if age>18:
+#     print("you have right to vote")   
+# else :
+#     print("you are underage")
+
+
+# age=int(input("enter age"))
+
+# if age<=19 and age >=13:
+#     print("you are a teenager") 
+# elif age>=20 and age<=30:
+#     print("you are young")
+# elif age>=31 and age <=40:
+#     print("you are an adult")   
+# elif age>=41 and age <=50:
+#     print("you are old")
+# elif age>50 and age<=100:
+#     print("you are senior citizen")
+# else:
+#     print("wrong input")
+    
+    
+    
+# username=input("enter ur username")
+# password=input("enter ur password")
+
+
+# if username == "akshat" and password == "1234":
+#     print(" you are logged in")
+# else:
+#     print("wrong crediantials")
+    
+    
+#enter marks if marks is greater the 30 and less than 50 than it should print grade c
+
+
+
+# marks=int(input('enter ur marks'))
+
+
+
+# if marks >=30 and marks <=40:
+#     print(" D ")
+# elif marks >40 and marks <=50:
+#     print(" C ")
+# elif marks >50 and marks <=60:
+#     print(" B ")
+# elif marks >60 and marks <=70:
+#     print(" A ")
+# elif marks >70 and marks <=100:
+#     print(" A+ ")
+# else:
+#     print("duffer")
+    
+    
+# names=["akshat","ashish",'vikas']
+
+# # print(names[2])
+
+
+# for i in range(len(names)):
+#     if names[i]=="ashish":
+#         print("found at ",i)
+#         break
+
+balance =20000
+pin=223344
+
+if balance<=20000 and (pin ==1234 or pin == 4567) and pin !=223344:  
+    print("allowed")      
+else:
+    print("not allowed")
