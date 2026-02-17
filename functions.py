2355d38438a2660b4c0157d2c61aaf6e23309f51
Akshat Saini <akshatsaini497@gmail.com>
2026-02-17 16:31:46 +0530

    lists functions

---
+++ functions.py
@@ -0,0 +1,62 @@
+# when we dont want to write the same code again and again we use functions
+
+
+
+# def add(): #defination of a function
+#     c=10+20
+#     
+    
+# add()#calling of a function
+
+# add()
+# add()
+
+
+# types of functions 
+
+# returning function
+# anaonymous
+# parametrised functions
+# lamda functions
+
+#anonymous functions are those function which doest have any name
+
+
+
+
+
+# returning functions are those function which return some value
+
+
+# def summition():
+#     return 5*5
+
+
+
+# sum=summition()
+
+# print(summition())
+
+# def add(a,b):#parameters
+   
+#     return a+b
+    
+    
+# print(add(10,20)) #arguments
+
+# print(add(100,20))
+# print(add(10,200))
+
+
+# def multi(x):
+#     return x*x
+
+# multiply=lambda x:x*x
+
+
+# print(multiply(30))
+
+
+
+
+
---
+++ lists.py
@@ -0,0 +1,17 @@
+#.      0.      1.  2.    3
+# array=["akshat","akshat",24,24.12,False]
+
+# array.append("vikas")
+
+# array.pop(2)
+# array.remove()
+# array.clear()
+# array.insert(0,"saini")
+#count
+
+# a=array.count("akshat")
+# print(len(array))
+# print(a)
+# lst=[12,11,2,4,55]
+# lst.sort(reverse=False)
+
