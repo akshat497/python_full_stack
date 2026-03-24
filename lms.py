25df2df504d33b111d77782f1183439f60e35f4a
Akshat Saini <akshatsaini497@gmail.com>
2026-03-24 15:38:00 +0530

    unenroll

--- lms.py
+++ lms.py
@@ -55,33 +55,57 @@ def addCourse():
     writeStudent=open("Courses.json","w")
     json.dump(Courses,writeStudent,indent=2)    
  
-def enroll():
+def enroll(task):
     students=getStudents()
     courses=getCourse()
-    
     studentsId=int(input("enter your students id"))
-    
-    print(courses)
-    
     courseId=int(input("enter the course id"))
-    
-    for student in students:
-        if student["id"]==studentsId:
-            student['courses'].append(courseId)
+    if task=="enroll":
+          for student in students:
+            if student["id"]==studentsId:
+              student['courses'].append(courseId)
+              print("enrolled successfully")
+    elif task=="unEnroll":
+          for student in students:
+            if student["id"]==studentsId:
+              student['courses'].remove(courseId)
+              print("unEnrolled successfully")
+            
+  
             
             
     writeStudent=open("students.json","w")
     json.dump(students,writeStudent,indent=2)   
+
+
+
+def searchStudent():
+    students=getStudents()
+    searchName=input("enter the student name you want to search")
     
+    for student in students:
+        if student['name']==searchName:
+            print(student)
+    
+    
+def deleteStudent():
+    students=getStudents()
+    id=int(input("enter the student id u want to delete"))  
+    for student in students:
+        if student['id']==id:
+            students.remove(id)
+     
+       
 while True:
     print("press 1 to add student")
     print("press 2 to add course")
     print("press 3 to view students")
     print("press 4 to view courses")
     print("press 5 to enroll ")
-    print("press 6 to update")
-    print("press 1 to add student")
-    print("press 1 to add student")
+    print("press 6 to unEnroll")
+    print("press 7 search  student")
+    print("press 8 to delete student")
+    print("press 9 to delete course")
     
     
     number=int(input("enter the number"))
@@ -90,4 +114,8 @@ while True:
     if number == 2:
         addCourse()
     if number==5:
-        enroll()
+        enroll("enroll")
+    if number==6:
+        enroll("unEnroll")
+    if number==7:
+        searchStudent()
